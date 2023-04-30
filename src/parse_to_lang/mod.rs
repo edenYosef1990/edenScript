e0651e4a6ef555b680b696e807e490b22336187b
eden <eden@DESKTOP-SRBRJOQ>
2023-04-30 23:16:30 +0300

    added code and stuff

--- src/parse_to_lang/mod.rs
+++ src/parse_to_lang/mod.rs
@@ -1,5 +1,34 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::{AddAssign, MulAssign}};
 
+
+pub struct PropertiesGroup{
+    pub components_names: Vec<String>
+}
+
+// it works given 99 components in total in game
+pub struct CompilerState{
+    component_to_id_dict : HashMap<String,u32>,
+    components_group_to_id: HashMap<u32,u32>
+}
+
+impl CompilerState {
+    pub fn hash_array_size_n(mut array: Vec<u32>) -> u32{
+        let mut index : u32 = 0;
+        array.sort();
+        for item in array {
+            index.mul_assign(100);
+            index.add_assign(item);
+        }
+        index
+    }
+
+    pub fn components_group_to_id(&self, components_group: &PropertiesGroup) -> u32{
+        let components_ids : Vec<u32> = components_group.components_names.iter()
+        .map(|component_name| self.component_to_id_dict.get(component_name).unwrap().clone()).collect();
+        let group_id = CompilerState::hash_array_size_n(components_ids);
+        self.components_group_to_id.get(&group_id).unwrap().clone()
+    }
+}
 
 pub struct GlobalObject {
     properties_dict: HashMap<String,u32>
