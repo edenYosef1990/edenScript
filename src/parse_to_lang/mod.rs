3d840a728654711f6f3eaa8fc9c6f8b07ad54427
Eden Yosef <bla@a.com>
2023-05-01 09:00:23 +0300

    more code

--- src/parse_to_lang/mod.rs
+++ src/parse_to_lang/mod.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashMap, ops::{AddAssign, MulAssign}};
+use std::{collections::HashMap, ops::{AddAssign, MulAssign}, str::SplitWhitespace};
+
+use bevy::prelude::KeyCode;
 
 
 pub struct PropertiesGroup{
@@ -55,6 +57,38 @@ pub struct GameData {
     values_dict: HashMap<String,GlobalObject>
 }
 
+pub fn str_to_key_code(str: &str) -> KeyCode{
+    match str {
+        "A" => KeyCode::A,
+        "B" => KeyCode::B,
+        "C" => KeyCode::C,
+        "D" => KeyCode::D,
+        "E" => KeyCode::E,
+        "F" => KeyCode::F,
+        "G" => KeyCode::G,
+        "H" => KeyCode::H,
+        "I" => KeyCode::I,
+        "J" => KeyCode::J,
+        "K" => KeyCode::K,
+        "L" => KeyCode::L,
+        "M" => KeyCode::M,
+        "N" => KeyCode::N,
+        "O" => KeyCode::O,
+        "P" => KeyCode::P,
+        "Q" => KeyCode::Q,
+        "R" => KeyCode::R,
+        "S" => KeyCode::S,
+        "T" => KeyCode::T,
+        "U" => KeyCode::U,
+        "V" => KeyCode::V,
+        "W" => KeyCode::W,
+        "X" => KeyCode::X,
+        "Y" => KeyCode::Y,
+        "Z" => KeyCode::Z,
+        _ => panic!("invalid input!")
+    }
+}
+
 fn get_lines_by_indentation_level(lines: &Vec<String> , start: usize, indentation_level: usize) -> (usize,usize){
     let end_index = lines[start..].iter()
         .position(|line| line.starts_with(&"\t".repeat(indentation_level)));
@@ -62,6 +96,16 @@ fn get_lines_by_indentation_level(lines: &Vec<String> , start: usize, indentatio
         return (start.clone() , end_index);
     }
     (start.clone(), lines.len())
+}
+
+fn parse_set_press(mut words_after_press_keyword : SplitWhitespace){
+    if let Some("=>") = words_after_press_keyword.next() { }
+    else {panic!("invalid input!");}
+
+    if let Some(char) = words_after_press_keyword.next() {
+        let key_code = str_to_key_code(char);
+     }
+
 }
 
 pub fn parse(file_content: String){
@@ -79,7 +123,7 @@ pub fn parse(file_content: String){
             Some("setEvent") => {
                 let (start_scope, end_scope) = get_lines_by_indentation_level(&lines_vec, pos+1, 1);
             },
-            Some("press") => {},
+            Some("setPress") => {parse_set_press(words)},
             _ => {}
         }
 
